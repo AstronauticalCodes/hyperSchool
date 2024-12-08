040c8d5aa0d2c96117e6954ea758ed919cc95789
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-08 16:37:43 +0530

    Completed-Stage-1.
    Created and registered models.

--- HyperSchool/task/schedule/models.py
+++ HyperSchool/task/schedule/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=60)
+    surname = models.CharField(max_length=40)
+    age = models.IntegerField()
+    about = models.TextField(max_length=1000)
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    info = models.TextField(max_length=1000)
+    duration_months = models.IntegerField()
+    price = models.IntegerField()
+    teacher = models.ManyToManyField(Teacher)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=60)
+    surname = models.CharField(max_length=40)
+    age = models.IntegerField()
+    course = models.ManyToManyField(Course)
