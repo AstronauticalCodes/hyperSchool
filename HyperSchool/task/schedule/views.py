88449268811678a45cc7184f5d9597d3f60e4298
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-09 12:36:44 +0530

    STAGE-1-Added redirection to schedule/main page.

--- HyperSchool/task/schedule/views.py
+++ HyperSchool/task/schedule/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse(f'''
+    <a href="/schedule/main"><button>Go to main page</button></a>
+''')
+    # return redirect('schedule/main')
+
+
+class MainView(View):
+    def get(self, request):
+        return render(request, "schedule/main.html")
+    pass
