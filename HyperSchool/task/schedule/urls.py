f60f93b9c310fc222373b105713b4ed59af44fee
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-09 21:57:25 +0530

    STAGE-3-Fixed template for both teacher and course details.
    Fixed the wrong tags acc to the stage requirements.

--- HyperSchool/task/schedule/urls.py
+++ HyperSchool/task/schedule/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     # path('schedule/main/', views.MainView.as_view()),
     path('main/', views.MainPage, name='main'),
+    path('course_details/<int:pk>/', views.MainPage, name='main'),
+    path('teacher_details/<int:pk>/', views.MainPage, name='main'),
 ]
