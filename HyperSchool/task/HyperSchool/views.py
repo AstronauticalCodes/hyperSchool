8420343d2f996f79de6dc2bb00d434ade8b8b177
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-10 13:16:28 +0530

    STAGE-5-Added class view for login feature.

--- HyperSchool/task/HyperSchool/views.py
+++ HyperSchool/task/HyperSchool/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse(f'''
     <a href="/schedule"><button>Go to schedule page</button></a>
 ''')
     # return redirect('schedule/main')
+
+
+class HyperSchoolLoginView(LoginView):
+    template_name = "login.html"
+    success_url = 'schedule/main'
+    form = AuthenticationForm
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={'form': self.form})
