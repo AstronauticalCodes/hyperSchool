0daf943ec27c565648247d914d3e73ed6ab42a36
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-10 13:12:38 +0530

    STAGE-5-Added SignUp template and SignUpForm in forms.py.

--- HyperSchool/task/schedule/forms.py
+++ HyperSchool/task/schedule/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Student
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class SearchForm(forms.Form):
     query = forms.CharField(label="Search for Courses", max_length=100)
@@ -9,3 +11,9 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'surname', 'age', 'course']
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
