dfc30b7300c3dc5a22b78ed753a709db9d74a73f
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-10 13:29:26 +0530

    STAGE-5-Added class view for signup and logout feature.

--- HyperSchool/task/HyperSchool/views.py
+++ HyperSchool/task/HyperSchool/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from HyperSchool.task.schedule.forms import SignUpForm
+
 
 def index(request):
     return HttpResponse(f'''
@@ -17,3 +22,32 @@ class HyperSchoolLoginView(LoginView):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'form': self.form})
+
+
+class HyperSchoolSignUpView(CreateView):
+    template_name = "signup.html"
+    success_url = "login"
+    form = UserCreationForm
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={'form': self.form})
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from.db()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+        else:
+            print('not valid')
+            return HttpResponse("<h2>not vaild</h2>")
+
+
+def LogOut(request):
+    logout(request)
+    return redirect('schedule/main')
