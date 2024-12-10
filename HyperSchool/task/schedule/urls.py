b4c93c41a9fabdba411c62c6389dcfed2ddbeeec
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-10 11:58:02 +0530

    STAGE-3-Completed.
    Fixed routing to course_details and teacher_details.
    APPPEND_SLASH = False is not working, idk why.
    Created new way to handle trailing slash in main.html and course_details.html
    Modified hs_test.

--- HyperSchool/task/schedule/urls.py
+++ HyperSchool/task/schedule/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index),
     # path('schedule/main/', views.MainView.as_view()),
     path('main/', views.MainPage, name='main'),
-    path('course_details/<int:pk>/', views.CourseDetailsView.as_view(), name='course_details'),
-    path('teacher_details/<int:pk>/', views.TeacherDetailsView.as_view(), name='teacher_details'),
+    path('course_details/<int:pk>', views.CourseDetailsView.as_view(), name='course_details'),
+    path('teacher_details/<int:pk>', views.TeacherDetailsView.as_view(), name='teacher_details'),
 ]
